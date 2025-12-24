6c43bdd67b743d4034ae70ad32ccf9330283131b
CryptoCoBean <rahulskater2001@gmail.com>
2025-12-24 11:57:14 +0400

    Doc update

---
+++ ps_setup.py
@@ -0,0 +1,19 @@
+api_name = 'REDACTED' 
+api_key = 'REDACTED' # API key from Etherscan V2 API
+
+NAMED_ADDRESSES = { # For EVM wallets
+    "Name #1": "REDACTED", 
+    "Name #2": "REDACTED",
+}
+
+SOLANA_ADDRESSES = { # For Solana wallets
+    "Name #1": "REDACTED", 
+    "Name #2": "REDACTED",
+}
+
+HYPERLIQUID_ADDRESSES = { # For Hypercore wallets
+    "Name #1": "REDACTED", 
+    "Name #2": "REDACTED",
+}
+
+portfolio_balances_fp = r'REDACTED' # Save file location of combined final CSV
