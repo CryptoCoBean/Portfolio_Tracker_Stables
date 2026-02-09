82e6fde0b5744109a0bf9fac929dd5d84290643a
CryptoCoBean <cryptocobean@gmail.com>
2026-02-09 18:22:48 +0000

    Added MegaEth Chain

--- config.py
+++ config.py
@@ -46,6 +46,25 @@ CHAINS = {
             },
         },
     },
+
+        "MegaETH": {
+        "chainid": 	4326,
+        "tokens": {
+            # "USDC": {
+            #     "address": "0xb88339CB7199b77E23DB6E890353E22632Ba630f",
+            #     "decimals": 6,
+            # },
+            "USDT": {
+                "address": "0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
+                "decimals": 6,
+            },
+            "USDm": {
+                # USDm on MegaEth
+                "address": "0xFAfDdbb3FC7688494971a79cc65DCa3EF82079E7",
+                "decimals": 6,
+            },
+        },
+    },
 }
 
 
